aff84eb608655a3b38694bc4fd2ad94024d92adb
Mark <mpower@tfd.ie>
2020-03-05 16:50:53 -0500

    Connect user score endpoint

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,9 @@
 <template>
     <div>
-        <aquire v-if="fakeData === 'aquire'"/>
-        <convert v-if="fakeData === 'convert'"/>
-        <adopt v-if="fakeData === 'adopt'"/>
-        <engage v-if="fakeData === 'engage'"/>
-
+        <aquire v-if="userScore >= 0.0 && userScore <= 2.5" />
+        <convert v-if="userScore >= 2.6 && userScore <= 5.0"/>
+        <adopt v-if="userScore >= 5.1 && userScore <= 7.5"/>
+        <engage v-if="userScore >= 7.6 && userScore <= 10"/>
     </div>
 </template>
 
@@ -13,7 +12,7 @@ import Convert from '@/subViews/Convert.vue'
 import Adopt from '@/subViews/Adopt.vue'
 import Engage from '@/subViews/Engage.vue'
 import Aquire from '@/subViews/Aquire.vue'
-// import axios from 'axios'
+import axios from 'axios'
 export default {
     name: 'Home',
     components: {
@@ -30,11 +29,11 @@ export default {
       userScore: 0
     }
   },
-  // mounted () {
-  //   axios
-  //     .get(this.endpoint + this.email)
-  //     .then(response => (this.userScore = response))
-  // }
+  mounted () {
+    axios
+      .get(this.endpoint + this.email)
+      .then(response => (this.userScore = response))
+  }
 }
 </script>
 
